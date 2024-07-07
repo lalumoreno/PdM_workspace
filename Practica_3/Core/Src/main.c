3eeba3bb3e6196ffef0d1c401de0bd5822a5d1b1
lalumoreno <moreno.rodriguez.laura@gmail.com>
2024-07-07 13:41:59 -0500

    Practica 3 (#3)

---
+++ Practica_3/Core/Src/main.c
@@ -0,0 +1,148 @@
+/**
+ ******************************************************************************
+ * @file           : main.c
+ * @brief          : Main program body
+ ******************************************************************************
+ * @attention
+ *
+ * Copyright (c) 2024 STMicroelectronics.
+ * All rights reserved.
+ *
+ * This software is licensed under terms that can be found in the LICENSE file
+ * in the root directory of this software component.
+ * If no LICENSE file comes with this software, it is provided AS-IS.
+ *
+ ******************************************************************************
+ */
+
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+#include "string.h"
+#include "stm32f4xx_nucleo_144.h" 	/* <- BSP include */
+#include "API_delay.h"				/* <- Timer include*/
+
+/* Private variables ---------------------------------------------------------*/
+const uint32_t TIEMPOS[]={500, 100, 100, 1000};
+const uint8_t MAXSEQ = sizeof(TIEMPOS) / sizeof(uint32_t);
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+
+/* Private user code ---------------------------------------------------------*/
+
+/**
+ * @brief  The application entry point.
+ * @retval  0 on success
+ */
+int main(void)
+{
+	/* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+	HAL_Init();
+
+	/* Configure the system clock */
+	SystemClock_Config();
+
+	/* Initialize BSP Led for LED1 and LED2 */
+	BSP_LED_Init(LED1);
+
+	/* Initialize timer for point 2 */
+	uint8_t index = 0;
+	delay_t timer;
+	bool_t ledState = false;
+	delayInit(&timer, TIEMPOS[index]);
+
+	/* Infinite loop */
+	while (1)
+	{
+		if(delayRead(&timer)){
+			// Change LED state
+			BSP_LED_Toggle(LED1);
+			ledState = ! ledState;
+
+			// If the LED was already turned on and off, change the duration of the timer
+			if(ledState == true){
+				index = (index < MAXSEQ - 1) ? index + 1 : 0; //Increase index or back to 0
+
+				// Point 3. Change the timer duration only if it is not running
+				if(!delayIsRunning(&timer)){
+					delayWrite(&timer, TIEMPOS[index]);
+				}
+			}
+		}
+	}
+
+	return 0;
+}
+
+/**
+ * @brief System Clock Configuration
+ * @retval None
+ */
+void SystemClock_Config(void)
+{
+	RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+	RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+	/** Configure the main internal regulator output voltage
+	 */
+	__HAL_RCC_PWR_CLK_ENABLE();
+	__HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
+
+	/** Initializes the RCC Oscillators according to the specified parameters
+	 * in the RCC_OscInitTypeDef structure.
+	 */
+	RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+	RCC_OscInitStruct.HSEState = RCC_HSE_BYPASS;
+	RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+	RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+	RCC_OscInitStruct.PLL.PLLM = 4;
+	RCC_OscInitStruct.PLL.PLLN = 168;
+	RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
+	RCC_OscInitStruct.PLL.PLLQ = 7;
+	if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+	{
+		Error_Handler();
+	}
+
+	/** Initializes the CPU, AHB and APB buses clocks
+	 */
+	RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+			|RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+	RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+	RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+	RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV4;
+	RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV2;
+
+	if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_5) != HAL_OK)
+	{
+		Error_Handler();
+	}
+}
+
+/**
+ * @brief  This function is executed in case of error occurrence.
+ * @retval None
+ */
+void Error_Handler(void)
+{
+	/* User can add his own implementation to report the HAL error return state */
+	__disable_irq();
+	while (1)
+	{
+	}
+}
+
+#ifdef  USE_FULL_ASSERT
+/**
+ * @brief  Reports the name of the source file and the source line number
+ *         where the assert_param error has occurred.
+ * @param  file: pointer to the source file name
+ * @param  line: assert_param error line source number
+ * @retval None
+ */
+void assert_failed(uint8_t *file, uint32_t line)
+{
+	/* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+}
+#endif /* USE_FULL_ASSERT */
