efd7ae8b5fe889b1266484d0da1039fe10309e50
Laura <moreno.rodriguez.laura@gmail.com>
2024-07-20 10:35:39 -0500

    Initial commit

---
+++ Practica_4/Core/Src/main.c
@@ -0,0 +1,191 @@
+/* USER CODE BEGIN Header */
+/**
+ ******************************************************************************
+ * @file           : main.c
+ * @brief          : Main program body
+ ******************************************************************************
+ * @attention
+ *
+ * Copyright (c) 2024 STMicroelectronics.
+ * All rights reserved.
+ *
+ * This software is licensed under terms that can be found in the LICENSE file
+ * in the root directory of this software component.
+ * If no LICENSE file comes with this software, it is provided AS-IS.
+ *
+ ******************************************************************************
+ */
+/* USER CODE END Header */
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+#include "API_debounce.h"
+#include "stm32f4xx_nucleo_144.h"
+#include "API_delay.h"
+
+/* Private typedef -----------------------------------------------------------*/
+
+/* Private define ------------------------------------------------------------*/
+
+/* Private macro -------------------------------------------------------------*/
+
+/* Private variables ---------------------------------------------------------*/
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+static void MX_GPIO_Init(void);
+
+/* Private user code ---------------------------------------------------------*/
+static delay_t delay;
+static delay_t delayLED;
+/**
+ * @brief  The application entry point.
+ * @retval int
+ */
+int main(void)
+{
+	/* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+	HAL_Init();
+
+	/* Configure the system clock */
+	SystemClock_Config();
+
+	// Inicializo la FSM
+	debounceFSM_init();
+
+	// Delay del antirebote
+	delayInit(&delay, 40);
+
+	// Frecuencias
+
+	const uint32_t TIEMPOS[] = {100, 500};
+
+	uint32_t index = 0;
+
+	// Delay del LED2
+	delayInit(&delayLED, TIEMPOS[0]);
+
+	// Periféricos
+	BSP_PB_Init(BUTTON_USER, BUTTON_MODE_GPIO);
+	BSP_LED_Init(LED1);
+	BSP_LED_Init(LED2);
+
+	while (1) {
+
+		if (delayRead(&delay)) {
+
+			// Lógica antirebote
+			if (BSP_PB_GetState(BUTTON_USER) == true) {
+				writeKey();
+			}
+
+			debounceFSM_update();
+
+		}
+
+		if (delayRead(&delay)) {
+
+			// Punto 4.1
+			if (isButtonDown()) {
+
+				BSP_LED_On(LED1);
+
+			} else {
+
+				BSP_LED_Off(LED1);
+
+			}
+
+
+		}
+
+		if (delayRead(&delayLED)) {
+
+			BSP_LED_Toggle(LED2);
+
+		}
+
+		if (isButtonDown()) {
+
+			delayWrite(&delayLED, TIEMPOS[index]);
+			index = (index + 1) % (sizeof(TIEMPOS) / sizeof(TIEMPOS[0]));
+
+		}
+
+
+	}
+
+}
+
+/**
+ * @brief System Clock Configuration
+ * @retval None
+ */
+void SystemClock_Config(void)
+{
+	RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+	RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+	/** Configure the main internal regulator output voltage
+	 */
+	__HAL_RCC_PWR_CLK_ENABLE();
+	__HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE3);
+
+	/** Initializes the RCC Oscillators according to the specified parameters
+	 * in the RCC_OscInitTypeDef structure.
+	 */
+	RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+	RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+	RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+	RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
+	if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+	{
+		Error_Handler();
+	}
+
+	/** Initializes the CPU, AHB and APB buses clocks
+	 */
+	RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+			|RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+	RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_HSI;
+	RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+	RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
+	RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+
+	if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0) != HAL_OK)
+	{
+		Error_Handler();
+	}
+}
+
+
+/**
+ * @brief  This function is executed in case of error occurrence.
+ * @retval None
+ */
+void Error_Handler(void)
+{
+	/* USER CODE BEGIN Error_Handler_Debug */
+	/* User can add his own implementation to report the HAL error return state */
+	__disable_irq();
+	while (1)
+	{
+	}
+	/* USER CODE END Error_Handler_Debug */
+}
+
+#ifdef  USE_FULL_ASSERT
+/**
+ * @brief  Reports the name of the source file and the source line number
+ *         where the assert_param error has occurred.
+ * @param  file: pointer to the source file name
+ * @param  line: assert_param error line source number
+ * @retval None
+ */
+void assert_failed(uint8_t *file, uint32_t line)
+{
+	/* USER CODE BEGIN 6 */
+	/* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+	/* USER CODE END 6 */
+}
+#endif /* USE_FULL_ASSERT */
---
+++ Practica_4/Drivers/API/Inc/API_debounce.h
@@ -0,0 +1,26 @@
+/*
+ * API_debounce.h
+ *
+ *  Created on: Jul 11, 2024
+ *      Author: laura
+ */
+
+#ifndef API_INC_API_DEBOUNCE_H_
+#define API_INC_API_DEBOUNCE_H_
+
+#include <stdbool.h>
+
+typedef bool bool_t;
+
+
+void debounceFSM_init(void);
+
+void debounceFSM_update(void);
+
+void writeKey(void);
+
+bool_t readKey();
+
+bool_t isButtonDown();
+
+#endif /* API_INC_API_DEBOUNCE_H_ */
